971d78d2715f5757b20088bf22f657ac4ee1cb4c
Niklas <niklas.kalnov@gmail.com>
2021-10-28 10:54:49 +0200

    harro
    shing shong

--- RezptzutatAbfrage.sql
+++ RezptzutatAbfrage.sql
@@ -1,4 +1,5 @@
 use krautundrueben;
 select distinct rezepte.REZEPTNAME as 'Rezept', zutaten.ZUTAT as 'Zutat' from rezeptzutaten, rezepte, zutaten
 WHere REZEPTZUTATEN.ZID = Zutaten.ZID
+and REZEPTZUTATEN.RID = REZEPTE.RID
 order by rezept asc
